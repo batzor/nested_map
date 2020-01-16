d5c5805eefb3f35f7fe173867c59d3e2e7657ea7
Batzorig Zorigoo <batzorig@kaist.ac.kr>
2020-01-16 21:16:41 +0900

    Add simple test for same bucket insertion

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -1,6 +1,6 @@
-use rayon::prelude::*;
-use nested_map::nested_map::NestedMap;
 use crossbeam_epoch::pin;
+use nested_map::nested_map::NestedMap;
+use rayon::prelude::*;
 
 #[test]
 fn smoke() {
@@ -15,8 +15,31 @@ fn smoke() {
 }
 
 #[test]
+fn insert_same_bucket() {
+    let map = NestedMap::default();
+    assert!(map.insert(91, 1, &pin()).is_none());
+    assert!(map.insert(256, 2, &pin()).is_none());
+    assert_eq!(map.lookup(&91, &pin()), Some(&1));
+    assert_eq!(map.lookup(&256, &pin()), Some(&2));
+    assert!(map.delete(&91, &pin()).is_ok());
+    assert_eq!(map.lookup(&91, &pin()), None);
+    assert_eq!(map.lookup(&256, &pin()), Some(&2));
+}
+
+#[test]
+fn insert_many() {
+    let map = NestedMap::default();
+
+    for i in 0..100000 {
+        assert!(map.insert(i, i * 5, &pin()).is_none());
+    }
+    for i in 0..100000 {
+        assert_eq!(map.lookup(&i, &pin()), Some(&(i * 5)));
+    }
+}
+
+#[test]
 fn insert_rayon() {
-    const INSV: u64 = 518;
     let map = NestedMap::default();
 
     (0..10000).into_par_iter().for_each(|i| {
